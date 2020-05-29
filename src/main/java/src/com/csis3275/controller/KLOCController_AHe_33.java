c762a2fda9e9a777401cc015a0b2cb158cf7a3b6
Aram Hernandez <hernandezgardunoa@student.douglascollege.ca>
2020-05-29 10:24:18 -0700

    Controller Added

--- src/main/java/src/com/csis3275/controller/KLOCController_AHe_33.java
+++ src/main/java/src/com/csis3275/controller/KLOCController_AHe_33.java
@@ -13,14 +13,14 @@ import src.com.csis3275.model.KLOC_AHe_33;
 @Controller
 //Here we will have this controller respond to requests from /employee for the employee servlet.
 @RequestMapping("/employee")
-public class EmployeeController {
+public class KLOCController_AHe_33 {
  /*
  * This method will serve as default GET handler.
  */
  @RequestMapping(method = RequestMethod.GET)
  public String newProfile(ModelMap model) {
- KLOC_AHe_33 employee = new KLOC_AHe_33();
- model.addAttribute("employee", employee);
+ KLOC_AHe_33 count = new KLOC_AHe_33();
+ model.addAttribute("count", count);
  return "employee";
  }
  /*
@@ -28,33 +28,18 @@ public class EmployeeController {
  * also validates the user input
  */
  @RequestMapping(method = RequestMethod.POST)
- public String saveProfile(KLOC_AHe_33 employee,
+ public String saveProfile(KLOC_AHe_33 count,
  BindingResult result, ModelMap model) {
 	 
  if (result.hasErrors()) {
  return "employee";
  }
  
- model.addAttribute("success", "Dear " + employee.getFirstName()
- + " , your profile completed successfully");
- model.addAttribute("employee",employee);
+ model.addAttribute("success", "total is: " + count.getResult()  );
+ model.addAttribute("count",count);
  return "success";
  }
  
  
- /*
- * Method used to populate the country list in view. Note that here you can
- * call external systems to provide real data.
- */
- @ModelAttribute("countries")
- public List<String> initializeCountries() {
- List<String> countries = new ArrayList<String>();
- countries.add("USA");
- countries.add("Canada");
- countries.add("France");
- countries.add("Indonesia");
- countries.add("Australia");
- countries.add("Other");
- return countries;
- }
+
 }
