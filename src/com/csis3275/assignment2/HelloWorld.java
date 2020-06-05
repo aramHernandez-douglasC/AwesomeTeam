72361953c8dc8e7b3e94e123b8593cc8bc73670d
Aram Hernandez <hernandezgardunoa@student.douglascollege.ca>
2020-06-04 18:44:34 -0700

    Hello World Application Added

---
+++ src/com/csis3275/assignment2/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.csis3275.assignment2;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
