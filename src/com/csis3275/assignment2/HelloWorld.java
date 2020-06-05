b1dfc7f6c61a061adab11d9b96272603923f5258
Phu Quy Tran <tranp6@student.douglascollege.ca>
2020-06-04 18:53:30 -0700

    Added my name: Ryan

--- src/com/csis3275/assignment2/HelloWorld.java
+++ src/com/csis3275/assignment2/HelloWorld.java
@@ -5,7 +5,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("");
-
+		
+		Member member2 = new Member();
+		
+		member2.setName("Ryan");
+		
+		System.out.println(member2.getName());
+	
 	}
 
 }
